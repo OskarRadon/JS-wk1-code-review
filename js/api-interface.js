82de33d936ebd49322f7cd235c5357f8e7b49b68
Oskar Radon <oskarradonkimball@gmail.com>
2016-03-18 08:33:10 -0700

    Display repo description

--- js/api-interface.js
+++ js/api-interface.js
@@ -7,8 +7,8 @@ $(document).ready(function() {
 
     $.get('https://api.github.com/users/' + username + '/repos?access_token=' + apiKey).then(function(data) {
       for (var i in data) {
-        // console.log(data[i].name);
-        $('.repoList').append('<li><a href="' + data[i].html_url + '">' + data[i].name + '</li></a>');
+        // console.log(data);
+        $('.repoList').append('<li><a href="' + data[i].html_url + '">' + data[i].name + '</li></a><p>' + data[i].description + '</p>');
       }
 
     }).fail(function(error) {
